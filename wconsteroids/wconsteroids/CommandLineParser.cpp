7e8045d7368bdb1936c2aa4e2bc3601da565c012
pacmaninbw <paul.chernick@chernicksw.com>
2022-10-30 13:21:42 -0700

    Added members to CommandLineParser Class

--- wconsteroids/wconsteroids/CommandLineParser.cpp
+++ wconsteroids/wconsteroids/CommandLineParser.cpp
@@ -1,1 +1,22 @@
 #include "CommandLineParser.h"
+#include <string>
+#include <vector>
+#include "cmdlineoutputtypes.h"
+
+CommandLineParser::CommandLineParser(int argc, char* argv[])
+	: argCount { argc }, args{ argv }
+{
+
+}
+
+void CommandLineParser::parse()
+{
+
+}
+
+CmdLineOutputTypes CommandLineParser::getOutputTypes()
+{
+	CmdLineOutputTypes outputTypes = CmdLineOutputTypes::Default;
+
+	return outputTypes;
+}
--- wconsteroids/wconsteroids/CommandLineParser.h
+++ wconsteroids/wconsteroids/CommandLineParser.h
@@ -1,8 +1,21 @@
 #ifndef COMMANDlINEPARSER_H
 #define COMMANDlINEPARSER_H
 
+#include <string>
+#include <vector>
+#include "cmdlineoutputtypes.h"
+
 class CommandLineParser
 {
+public:
+	CommandLineParser(int argc, char* argv[]);
+	CmdLineOutputTypes getOutputTypes();
+	void parse();
+
+private:
+	std::vector<std::string> arguments;
+	char** args;
+	int argCount;
 };
 
 #endif // COMMANDlINEPARSER_H
