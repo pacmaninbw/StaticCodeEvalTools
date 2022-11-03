af8a0f0a60be18c631f59878f314c3878b498855
pacmaninbw <paul.chernick@chernicksw.com>
2022-11-03 13:50:57 -0700

    Added 2 new functions to provide headers for output.

--- wconsteroids/wconsteroids/ReportWriter.cpp
+++ wconsteroids/wconsteroids/ReportWriter.cpp
@@ -1,15 +1,99 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include "ReportWriter.h"
 
 void ReportWriter::printResult(FileStatistics& resultsForOutput)
 {
-
+	std::cout << getResultText(resultsForOutput) << "\n";
 }
 
 std::string ReportWriter::getResultText(FileStatistics& resultsForOutput)
 {
 	std::string outString;
 
+	outString = resultsForOutput.getFileName();
+	outString += ":\t";
+
 	return outString;
 }
 
+std::vector<std::string> ReportWriter::getColumneHeadingsText()
+{
+	std::string firstLine;
+	std::string secondline;
+
+	if (executionCtrl->lineCount)
+	{
+		firstLine += "Lines\t";
+		secondline += "of Text\t";
+	}
+
+	if (executionCtrl->wordCount)
+	{
+		firstLine += "Words\t";
+		secondline += "\t";
+	}
+
+	if (executionCtrl->byteCount)
+	{
+		firstLine += "Bytes\t";
+		secondline += "\t";
+	}
+
+	if (executionCtrl->charCount)
+	{
+		firstLine += "Characters\t";
+		secondline += "\t\t";
+	}
+
+	if (executionCtrl->maxLineWidth)
+	{
+		firstLine += "Length of\t";
+		secondline += "Longest Line\t";
+	}
+
+	// End of backwards compatability with wc utility.
+
+	if (executionCtrl->codeCount)
+	{
+		firstLine += "Lines\t";
+		secondline += "of Code\t";
+	}
+
+	if (executionCtrl->commentCount)
+	{
+		firstLine += "Lines of\t";
+		secondline += "Comments\t";
+	}
+
+	if (executionCtrl->percentages)
+	{
+		firstLine += "Percentage of\t";
+		secondline += "Lines of Code\t";
+	}
+
+	if (executionCtrl->whitespaceCount)
+	{
+		firstLine += "Whitespace\t";
+		secondline += "Characters\t";
+	}
+	if (executionCtrl->blankLineCount)
+	{
+		firstLine += "Blank\t";
+		secondline += "Lines\t";
+	}
+
+
+	std::vector<std::string> headerRows = { firstLine, secondline };
+	return headerRows;
+}
+
+void ReportWriter::printColumnHedings()
+{
+	std::vector<std::string> headerRows = getColumneHeadingsText();
+	for (auto line : headerRows)
+	{
+		std::cout << line << "\n";
+	}
+}
--- wconsteroids/wconsteroids/ReportWriter.h
+++ wconsteroids/wconsteroids/ReportWriter.h
@@ -1,20 +1,33 @@
 #ifndef REPORTWRITER_H
 #define REPORTWRITER_H
 
-#include <string>
-#include "cmdlineoutputtypes.h"
+/*
+ * This class prints the output about the file statistics. It can also
+ * return a formated string of the output.
+ */
+#include <memory>
+#include <string>	// std::vector included by string
+#include "Executionctrlvalues.h"
 #include "FileStatistics.h"
 
 class ReportWriter
 {
 public:
-	ReportWriter(CmdLineOutputTypes inOutputTypes= CmdLineOutputTypes::Default)
-		:outputTypes { inOutputTypes } {}
+	// ExecutionCrtlValues is passed in so that the report writer know what
+	// output to generate.
+	ReportWriter(ExecutionCtrlValues& executionctrl)
+	{
+		executionCtrl = std::make_shared<ExecutionCtrlValues>(executionctrl);
+	}
 	void printResult(FileStatistics& resultsForOutput);
 	std::string getResultText(FileStatistics& resultsForOutput);
+	void printColumnHedings();
+	// Returns 2 lines of properly formated text
+	std::vector<std::string> getColumneHeadingsText();
 
 private:
-	CmdLineOutputTypes outputTypes;
+	std::shared_ptr <ExecutionCtrlValues> executionCtrl;
+
 };
 
 #endif // REPORTWRITER_H
