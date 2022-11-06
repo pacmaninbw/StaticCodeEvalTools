003e73e619a4854e23556777db293161760dd0b4
pacmaninbw <paul.chernick@chernicksw.com>
2022-11-06 06:05:24 -0800

    Added code to fake the command line arguments for unit testing and debugging.

--- wconsteroids/wconsteroids/main.cpp
+++ wconsteroids/wconsteroids/main.cpp
@@ -23,6 +23,29 @@ static void debugMainLoop(ExecutionCtrlValues& executionCtrl)
 		std::cout << "\t" << fileName << "\n";
 	}
 }
+
+static char** makeFakeArgs()
+{
+	char** fakeArgs = new char* [4];
+	std::string arg1("-Llcw");
+	std::string arg2("-R");
+	std::string arg3("*.cpp");
+	std::string arg4("*.h");
+
+	fakeArgs[0] = new char[arg1.length() + 1];
+	strcpy(fakeArgs[0], arg1.c_str());
+
+	fakeArgs[1] = new char[arg2.length() + 1];
+	strcpy(fakeArgs[1], arg2.c_str());
+
+	fakeArgs[2] = new char[arg3.length() + 1];
+	strcpy(fakeArgs[2], arg3.c_str());
+
+	fakeArgs[3] = new char[arg4.length() + 1];
+	strcpy(fakeArgs[3], arg4.c_str());
+
+	return fakeArgs;
+}
 #endif
 
 static void mainLoop(ExecutionCtrlValues& executionCtrl)
@@ -39,7 +62,10 @@ int main(int argc, char* argv[])
 	int exit_status = EXIT_SUCCESS;
 	ExecutionCtrlValues executionCtrl;
 	std::string versionString("1.0.0");
-	CommandLineParser cmdLineParser(argc, argv, versionString);
+
+	char** FAKEARGV = makeFakeArgs();
+	int FAKEARGC = sizeof(FAKEARGV) / sizeof(*FAKEARGV);
+	CommandLineParser cmdLineParser(4, FAKEARGV, versionString);
 
 	try
 	{
