a7073bddb23396f688923b08b3ea884ec1185a9a
pacmaninbw <paul.chernick@chernicksw.com>
2022-11-09 11:30:15 -0800

    Inplemented the word counting portion of the file parser. Also implemented the counting of white space.

--- wconsteroids/wconsteroids/FileParser.cpp
+++ wconsteroids/wconsteroids/FileParser.cpp
@@ -1,20 +1,89 @@
+#include <cctype>
+#include <iostream>
 #include <vector>
+#include <string>
+#include <sstream>
 #include "CharBuffer.h"
 #include "FileParser.h"
 #include "FileStatistics.h"
 
 FileParser::FileParser(FileStatistics& fileStats)
-	: fileStatistics{fileStats}
+	: fileStatistics{ fileStats },
+	inComment{ false },
+	inBlockComment{ false }
 {
 }
 
-void FileParser::ParseBuffer(CharBuffer& inputBuffer)
+void FileParser::ParseBuffer(CharBuffer& inputBuffer) noexcept
 {
 	while (!inputBuffer.endOfBuffer())
 	{
-		std::vector<char> line = inputBuffer.getCurrentLine();
+		std::vector<unsigned char> line = inputBuffer.getCurrentLine();
 		fileStatistics.addToCharCount(line.size());
 		fileStatistics.updateWidestLine(line.size());
 		fileStatistics.incrementTotalLines();
+		parseLine(line);
 	}
+}
+
+void FileParser::parseLine(std::vector<unsigned char> line) noexcept
+{
+	size_t whiteSpaceCount = 0;
+	std::vector<std::string> tokens = tokenize(line);
+	fileStatistics.addToWordCount(tokens.size());
+}
+
+static std::string tokenizeWord(std::vector<unsigned char>::iterator& currentChar)
+{
+	std::string token;
+
+	while (isalnum(*currentChar))
+	{
+		token.push_back(*currentChar++);
+	}
+
+	return token;
+}
+
+static std::string tokenizeOperator(std::vector<unsigned char>::iterator& currentChar)
+{
+	std::string token;
+
+	while ((!isalnum(*currentChar)) && (!isspace(*currentChar)))
+	{
+		token.push_back(*currentChar++);
+	}
+
+	return token;
+}
+
+std::vector<std::string> FileParser::tokenize(std::vector<unsigned char> line) noexcept
+{
+	std::vector<std::string> tokens;
+	std::vector<unsigned char>::iterator currentChar;
+	size_t whiteSpaceCount = 0;
+	std::string sline(line.begin(), line.end());
+
+	for (currentChar = line.begin(); *currentChar != '\n'; )
+	{
+		while (*currentChar != '\n' && isspace(*currentChar))
+		{
+			currentChar++;
+			whiteSpaceCount++;
+		}
+
+		if (*currentChar != '\n')
+		{
+			std::string token = (isalnum(*currentChar)) ? tokenizeWord(currentChar):
+				tokenizeOperator(currentChar);
+			if (!token.empty())
+			{
+				tokens.push_back(token);
+			}
+		}
+	}
+
+	fileStatistics.addToWhitespace(whiteSpaceCount);
+
+	return tokens;
 }
--- wconsteroids/wconsteroids/FileParser.h
+++ wconsteroids/wconsteroids/FileParser.h
@@ -1,6 +1,8 @@
 #ifndef FILEPARSER_H
 #define FILEPARSER_H
 
+#include <string>
+#include <vector>
 #include "CharBuffer.h"
 #include "FileStatistics.h"
 
@@ -8,14 +10,20 @@ class FileParser
 {
 public:
 	FileParser(FileStatistics& fileStats);
-	void ParseBuffer(CharBuffer& inputbuffer);
-	void addBufferStats(FileStatistics& processorStats)
+	void ParseBuffer(CharBuffer& inputbuffer) noexcept;
+	void addBufferStats(FileStatistics& processorStats) noexcept
 	{
 		fileStatistics.addTotals(processorStats);
 	}
 
+protected:
+	void parseLine(std::vector<unsigned char> line) noexcept;
+	std::vector<std::string> tokenize(std::vector<unsigned char> line) noexcept;
+
 private:
 	FileStatistics& fileStatistics;
+	bool inComment;
+	bool inBlockComment;
 };
 
 #endif // FILEPARSER_H
