f540180824c9e977a81eb9fdd697b11c19fa94ba
pacmaninbw <paul.chernick@chernicksw.com>
2022-11-17 14:39:33 -0800

    To reduce warning messages from the g++ compiler removed the variable names from the exceptions in the catch clauses.

--- wconsteroids/wconsteroids/main.cpp
+++ wconsteroids/wconsteroids/main.cpp
@@ -48,16 +48,16 @@ int main(int argc, char* argv[])
 		}
 	}
 
-	catch (ShowHelpMessage sh)
+	catch (ShowHelpMessage)
 	{
 		cmdLineParser.printHelpMessage();
 		cmdLineParser.printVersion();
 	}
-	catch (showVersions sv)
+	catch (showVersions)
 	{
 		cmdLineParser.printVersion();
 	}
-	catch (std::exception ex)
+	catch (std::exception &ex)
 	{
 		std::cerr << "Error: " << ex.what() << "\n";
 		exit_status = EXIT_FAILURE;
