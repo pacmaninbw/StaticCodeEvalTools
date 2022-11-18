3eac280394f5da7c54c562018bf6a66def9ef648
pacmaninbw <paul.chernick@chernicksw.com>
2022-11-17 16:49:32 -0800

    Following good programing practices the declarations for start and end were split onto 2 lines.

--- wconsteroids/wconsteroids/UtilityTimer.h
+++ wconsteroids/wconsteroids/UtilityTimer.h
@@ -32,7 +32,8 @@ public:
 	}
 
 private:
-	std::chrono::time_point<std::chrono::system_clock> start{}, end{};
+	std::chrono::time_point<std::chrono::system_clock> start{};
+	std::chrono::time_point<std::chrono::system_clock> end{};
 };
 
 #endif // CC_UTITLTY_TIMER_H
