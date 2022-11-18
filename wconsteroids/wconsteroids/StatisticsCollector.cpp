d58ac045bbf1bc59fd5db99387aa1c16d9d7a63e
Toby Speight <T.M.Speight.90@cantab.net>
2022-11-18 09:36:07 +0000

    Don't pass large strings by value

--- wconsteroids/wconsteroids/StatisticsCollector.cpp
+++ wconsteroids/wconsteroids/StatisticsCollector.cpp
@@ -15,10 +15,10 @@ StatisticsCollector::StatisticsCollector(FileStatistics& fileStats)
 {
 }
 
-void StatisticsCollector::analyzeBuffer(std::string inputBuffer) noexcept
+void StatisticsCollector::analyzeBuffer(std::string_view inputBuffer) noexcept
 {
-	std::string::iterator endBuffer = inputBuffer.end();
-	std::string::iterator startBuffer = inputBuffer.begin();
+	auto endBuffer = inputBuffer.end();
+	auto startBuffer = inputBuffer.begin();
 
 	std::uintmax_t bufferSize = std::filesystem::file_size(fileStatistics.getFileName());
 	fileStatistics.setCharCount(bufferSize);
@@ -27,24 +27,23 @@ void StatisticsCollector::analyzeBuffer(std::string inputBuffer) noexcept
 	fileStatistics.setToLineCount(lineCount);
 	countWordsAndWhiteSpace(inputBuffer);
 
-	std::string::iterator currentChar = startBuffer;
+	auto currentChar = startBuffer;
 	while (currentChar != endBuffer)
 	{
 		updateWidestLine(currentChar, endBuffer);
 	}
 }
 
-void StatisticsCollector::countWordsAndWhiteSpace(std::string& inputBuffer) noexcept
+void StatisticsCollector::countWordsAndWhiteSpace(std::string_view inputBuffer) noexcept
 {
 	std::size_t wordCount = 0;
 	std::size_t whiteSpaceCount = 0;
-	std::string::iterator currentChar = inputBuffer.begin();
-	std::string::iterator endOfInput = inputBuffer.end();
+	auto currentChar = inputBuffer.begin();
+	auto endOfInput = inputBuffer.end();
 	bool inWord = false;
-
 	for ( ; currentChar != endOfInput; )
 	{
-		while (isspace(*currentChar))
+            while (std::isspace(static_cast<unsigned char>(*currentChar)))
 		{
 			whiteSpaceCount++;
 			currentChar++;
@@ -77,10 +76,10 @@ void StatisticsCollector::countWordsAndWhiteSpace(std::string& inputBuffer) noex
 	fileStatistics.setWordCount(wordCount);
 }
 
-void StatisticsCollector::updateWidestLine(std::string::iterator& currentChar,
-	std::string::iterator end) noexcept
+void StatisticsCollector::updateWidestLine(std::string_view::const_iterator& currentChar,
+                                           std::string_view::const_iterator end) noexcept
 {
-	std::string::iterator endOfLine = std::find(currentChar, end, '\n');
+	auto endOfLine = std::find(currentChar, end, '\n');
 	if (endOfLine != end)
 	{
 		endOfLine++;
--- wconsteroids/wconsteroids/StatisticsCollector.h
+++ wconsteroids/wconsteroids/StatisticsCollector.h
@@ -2,21 +2,20 @@
 #define STATISTICS_COLLECTOR_H
 
 #include <iterator>
-#include <string>
-#include <vector>
 #include "FileStatistics.h"
+#include <string_view>
 
 class StatisticsCollector
 {
 public:
 	StatisticsCollector(FileStatistics& fileStats);
-	void analyzeBuffer(std::string inputbuffer) noexcept;
+	void analyzeBuffer(std::string_view inputBuffer) noexcept;
 
 protected:
-	void updateWidestLine(std::string::iterator& currentChar,
-		std::string::iterator end) noexcept;
-	void lineWidth(std::string line) noexcept;
-	void countWordsAndWhiteSpace(std::string& inputBuffer) noexcept;
+	void updateWidestLine(std::string_view::const_iterator& currentChar,
+                              std::string_view::const_iterator end) noexcept;
+	void lineWidth(std::string_view line) noexcept;
+	void countWordsAndWhiteSpace(std::string_view inputBuffer) noexcept;
 
 private:
 	FileStatistics& fileStatistics;
