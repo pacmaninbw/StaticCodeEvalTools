cf0fd8e4afe6401f2a4e00cba4a2eb849dbb28af
pacmaninbw <paul.chernick@chernicksw.com>
2022-11-26 09:54:15 -0800

    Added some unit tests for FileStatistics class. Created testFileStatistics.cpp in unitTest. Removed test cases from FileStatistics.cpp.

---
+++ wconsteroids/unitTest/testFileStatistics.cpp
@@ -0,0 +1,124 @@
+#include <doctest/doctest.h>
+
+DOCTEST_MAKE_STD_HEADERS_CLEAN_FROM_WARNINGS_ON_WALL_BEGIN
+#include "FileStatistics.h"
+DOCTEST_MAKE_STD_HEADERS_CLEAN_FROM_WARNINGS_ON_WALL_END
+
+
+// Guess estimates of the ratio of code to comments and blank lines
+// Also guess estimates of word count and characters per line
+static constexpr std::size_t totalLineCount = 617;
+static constexpr std::size_t commentLineCount =
+static_cast<std::size_t>(totalLineCount * 0.09);
+static constexpr std::size_t codeWithCommentCount =
+static_cast<std::size_t>(totalLineCount * 0.01);
+static constexpr std::size_t blankLineCount =
+static_cast<std::size_t>(totalLineCount / 10);
+static constexpr std::size_t codeLineCount = totalLineCount -
+(commentLineCount + codeWithCommentCount + blankLineCount);
+static constexpr std::size_t characterCount = totalLineCount * 51;
+static constexpr std::size_t wordCount = totalLineCount * 7;
+static constexpr std::size_t whiteSpaceCount = wordCount - 1;
+
+TEST_CASE("Test Constructors")
+{
+	SUBCASE("Constructor with name")
+	{
+		std::string name("Constructor has name");
+		FileStatistics constructorWithName(name);
+		CHECK(constructorWithName.getFileName().empty() == false);
+		CHECK(constructorWithName.getFileName() == name);
+		CHECK(constructorWithName.getBlankLines() == 0);
+		CHECK(constructorWithName.getCharacters() == 0);
+		CHECK(constructorWithName.getCodeLines() == 0);
+		CHECK(constructorWithName.getCodeWithComment() == 0);
+		CHECK(constructorWithName.getCommentLines() == 0);
+		CHECK(constructorWithName.getWhitespace() == 0);
+		CHECK(constructorWithName.getWidestLine() == 0);
+		CHECK(constructorWithName.getWords() == 0);
+		CHECK(constructorWithName.getTotalLines() == 0);
+		CHECK(constructorWithName.getPerecentageOfCode() == 0);
+	}
+
+	SUBCASE("Constructor with no name")
+	{
+		FileStatistics constructorWithNoName;
+		CHECK(constructorWithNoName.getFileName().empty() == true);
+		CHECK(constructorWithNoName.getBlankLines() == 0);
+		CHECK(constructorWithNoName.getCharacters() == 0);
+		CHECK(constructorWithNoName.getCodeLines() == 0);
+		CHECK(constructorWithNoName.getCodeWithComment() == 0);
+		CHECK(constructorWithNoName.getCommentLines() == 0);
+		CHECK(constructorWithNoName.getWhitespace() == 0);
+		CHECK(constructorWithNoName.getWidestLine() == 0);
+		CHECK(constructorWithNoName.getWords() == 0);
+		CHECK(constructorWithNoName.getTotalLines() == 0);
+		CHECK(constructorWithNoName.getPerecentageOfCode() == 0);
+	}
+}
+
+TEST_CASE("Test FileStatistics Increment Functions")
+{
+
+	FileStatistics testIncrement("Test Increment");
+
+	// First test every increment function individually
+
+	for (int i = 0; i < totalLineCount; i++)
+	{
+		testIncrement.incrementTotalLines();
+	}
+	CHECK(testIncrement.getTotalLines() == totalLineCount);
+
+	for (int i = 0; i < commentLineCount; i++)
+	{
+		testIncrement.incrementCommentsLines();
+	}
+	CHECK(testIncrement.getCommentLines() == commentLineCount);
+
+	for (int i = 0; i < codeWithCommentCount; i++)
+	{
+		testIncrement.incrementCodeWithComment();
+	}
+	CHECK(testIncrement.getCodeWithComment() == codeWithCommentCount);
+
+	for (int i = 0; i < blankLineCount; i++)
+	{
+		testIncrement.incrementBlankLines();
+	}
+	CHECK(testIncrement.getBlankLines() == blankLineCount);
+
+	for (int i = 0; i < codeLineCount; i++)
+	{
+		testIncrement.incrementCodeLines();
+	}
+	CHECK(testIncrement.getCodeLines() == codeLineCount);
+
+	for (int i = 0; i < whiteSpaceCount; i++)
+	{
+		testIncrement.incrementWhitespace();
+	}
+	CHECK(testIncrement.getWhitespace() == whiteSpaceCount);
+
+	for (int i = 0; i < characterCount; i++)
+	{
+		testIncrement.incrementCharacter();
+	}
+	CHECK(testIncrement.getCharacters() == characterCount);
+
+	for (int i = 0; i < wordCount; i++)
+	{
+		testIncrement.incrementWords();
+	}
+	CHECK(testIncrement.getWords() == wordCount);
+
+	// Now check to see if there were any side affects.
+	CHECK(testIncrement.getTotalLines() == totalLineCount);
+	CHECK(testIncrement.getCommentLines() == commentLineCount);
+	CHECK(testIncrement.getCodeWithComment() == codeWithCommentCount);
+	CHECK(testIncrement.getBlankLines() == blankLineCount);
+	CHECK(testIncrement.getCodeLines() == codeLineCount);
+	CHECK(testIncrement.getWhitespace() == whiteSpaceCount);
+	CHECK(testIncrement.getCharacters() == characterCount);
+	CHECK(testIncrement.getWords() == wordCount);
+}
