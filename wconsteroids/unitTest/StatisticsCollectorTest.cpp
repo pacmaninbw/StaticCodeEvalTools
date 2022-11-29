86835a0420d6b1153410fbcff5e2cf5b56f45210
pacmaninbw <paul.chernick@chernicksw.com>
2022-11-29 15:59:54 -0800

    All Unit tests for the  StatisticsCollector have been written, there are currently 2 bugs in the unit test that need to be fixed.

--- wconsteroids/unitTest/StatisticsCollectorTest.cpp
+++ wconsteroids/unitTest/StatisticsCollectorTest.cpp
@@ -4,10 +4,171 @@ DOCTEST_MAKE_STD_HEADERS_CLEAN_FROM_WARNINGS_ON_WALL_BEGIN
 #include <fstream>
 #include <filesystem>
 #include <string>
+#include <string_view>
 #include "FileStatistics.h"
 #include "StatisticsCollector.h"
 DOCTEST_MAKE_STD_HEADERS_CLEAN_FROM_WARNINGS_ON_WALL_END
 
+// This vector is input for the first 2 
+static std::vector<std::string_view> oneLineTestInput =
+{
+	"\n",												// 1 char, 1 white space no words
+	"Hello World\n",									// 12 char, 2 white space, 2 words
+	"Hello	World	\n",								// 2 tabs, 27 char, 3 white space, 2 words
+	"Very simple test, not code, no tabs\n",			// 36 char, 7 white space including new line , 7 words
+	"				\n",								// 4 tabs, 33 char including new line, 5 white space, no words
+	"               \n",								// no tabs, 16 char, 16 white space, no words
+	"This will be the widest line								\n", //8 tabs, // 93 char, 14 white space, 6 words
+};
+
+struct Test1ExpectedResults
+{
+	std::size_t charCount;
+	std::size_t wordCount;
+	std::size_t whiteSpaceCount;
+	std::size_t width;
+};
+
+static std::vector<struct Test1ExpectedResults> testOneExpectedValues =
+{
+	{1, 0, 1, 1},
+	{12, 2, 2, 12},
+	{13, 2, 3, 29},
+	{36, 7, 7, 36},
+	{5, 0, 5, 37},
+	{17, 0, 16, 16},
+	{37, 6, 14, 101},
+};
+constexpr std::size_t widestLineIndex = 6;
+
+static std::string mergeOneLineTests(Test1ExpectedResults& totals)
+{
+	std::size_t testIndex = 0;
+
+	std::string mergedTestInputStr;
+	for (auto line : oneLineTestInput)
+	{
+		mergedTestInputStr += line;
+		totals.wordCount += testOneExpectedValues[testIndex].wordCount;
+		totals.whiteSpaceCount += testOneExpectedValues[testIndex].whiteSpaceCount;
+		totals.charCount += testOneExpectedValues[testIndex].charCount;
+		testIndex++;
+	}
+
+	totals.width = testOneExpectedValues[widestLineIndex].width;
+
+	return mergedTestInputStr;
+}
+
+TEST_CASE("Statistics Collection Test 1: Count Words and White Space")
+{
+	SUBCASE("Sub Test 1: Test One Liners")
+	{
+		std::size_t testIndex = 0;
+		for (auto line : oneLineTestInput)
+		{
+			FileStatistics FSTestOne("one line");
+			StatisticsCollector SCTestOne(FSTestOne);
+			SCTestOne.countWordsAndWhiteSpace(line);
+			CHECK(FSTestOne.getWords() == testOneExpectedValues[testIndex].wordCount);
+			CHECK(FSTestOne.getWhitespace() == testOneExpectedValues[testIndex].whiteSpaceCount);
+			testIndex++;
+		}
+	}
+
+	SUBCASE("Sub Test 2: All One Liners Together")
+	{
+		Test1ExpectedResults totals = { 0 };
+		std::string temp = mergeOneLineTests(totals);
+		std::string_view mergedTestInput = temp;
+
+		FileStatistics FSTestOne("merged test");
+		StatisticsCollector SCTestOne(FSTestOne);
+		SCTestOne.countWordsAndWhiteSpace(mergedTestInput);
+		CHECK(FSTestOne.getWords() == totals.wordCount);
+		CHECK(FSTestOne.getWhitespace() == totals.whiteSpaceCount);
+	}
+}
+
+TEST_CASE("Statistics Collection Test 2: Widest Line")
+{
+	SUBCASE("Sub Test 3: Test Widest One Liners")
+	{
+		std::size_t testIndex = 0;
+		for (auto line : oneLineTestInput)
+		{
+			FileStatistics FSTestOne("one line");
+			StatisticsCollector SCTestOne(FSTestOne);
+			auto endBuffer = line.end();
+			auto startBuffer = line.begin();
+
+			SCTestOne.updateWidestLine(startBuffer, endBuffer);
+			CHECK(FSTestOne.getWidestLine() == testOneExpectedValues[testIndex].width);
+			testIndex++;
+		}
+	}
+
+	SUBCASE("Sub Test 4: Test Widest Line Merged")
+	{
+		Test1ExpectedResults totals = { 0 };
+		std::string temp = mergeOneLineTests(totals);
+		std::string_view mergedTestInput = temp;
+
+		FileStatistics FSTestOne("one line");
+		StatisticsCollector SCTestOne(FSTestOne);
+		auto endBuffer = mergedTestInput.end();
+		auto startBuffer = mergedTestInput.begin();
+
+		auto currentChar = startBuffer;
+		while (currentChar != endBuffer)
+		{
+			SCTestOne.updateWidestLine(currentChar, endBuffer);
+		}
+
+		CHECK(FSTestOne.getWidestLine() == testOneExpectedValues[widestLineIndex].width);
+	}
+}
+
+TEST_CASE("Statistics Collection Test 3: Analyze Buffer")
+{
+	SUBCASE("Sub Test 5: Analyze Buffer Test One Liners")
+	{
+		std::size_t testIndex = 0;
+		for (auto line : oneLineTestInput)
+		{
+			FileStatistics FSTestOne("one line");
+			StatisticsCollector SCTestOne(FSTestOne);
+			SCTestOne.analyzeBuffer(line);
+			CHECK(FSTestOne.getCharacters() == testOneExpectedValues[testIndex].charCount);
+			CHECK(FSTestOne.getWords() == testOneExpectedValues[testIndex].wordCount);
+			CHECK(FSTestOne.getWhitespace() == testOneExpectedValues[testIndex].whiteSpaceCount);
+			CHECK(FSTestOne.getWidestLine() == testOneExpectedValues[testIndex].width);
+			CHECK(FSTestOne.getTotalLines() == 1);
+			testIndex++;
+		}
+	}
+
+	SUBCASE("Sub Test 6: Analyze Buffer Merged Test")
+	{
+		Test1ExpectedResults totals = { 0 };
+		std::string temp = mergeOneLineTests(totals);
+		std::string_view mergedTestInput = temp;
+
+		FileStatistics FSTestOne("one line");
+		StatisticsCollector SCTestOne(FSTestOne);
+		SCTestOne.analyzeBuffer(mergedTestInput);
+		CHECK(FSTestOne.getCharacters() == totals.charCount);
+		CHECK(FSTestOne.getWords() == totals.wordCount);
+		CHECK(FSTestOne.getWhitespace() == totals.whiteSpaceCount);
+		CHECK(FSTestOne.getWidestLine() == totals.width);
+		CHECK(FSTestOne.getTotalLines() == testOneExpectedValues.size());
+	}
+}
+
+/*
+ * Final StatisticsCollector Unit Test
+ * Use actual source files to test the statistics collection.
+ */
 static const std::string testRoot = "wconsteroids";
 
 static bool FindUnitTestDirectorRoot(std::filesystem::path& testDir)
