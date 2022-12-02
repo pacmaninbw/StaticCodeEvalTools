4e4c5ba1d0d1ef3602da64f7835a3dfbfade192f
pacmaninbw <paul.chernick@chernicksw.com>
2022-12-02 11:07:40 -0800

    All  ProgramOptions normal functions tested except for printHelpMessage(), printVersion() and initFromEnvironmentVars(). I need to figure out how to test printHelpMessage(), and printVersion(). initFromEnvironmentVars() is currently just a stub.

--- wconsteroids/unitTest/ProgramOptionsTest.cpp
+++ wconsteroids/unitTest/ProgramOptionsTest.cpp
@@ -61,7 +61,6 @@ TEST_CASE("Program Options Unit Test 2: Set Defaults When No Flags")
 
 TEST_CASE("Program Options Unit Test 3: Single Dash Arguments")
 {
-
 	SUBCASE("Single Dash Arguments Sub Test 1")
 	{
 		// When a single output flag is set, only that output should be expected
@@ -133,6 +132,207 @@ TEST_CASE("Program Options Unit Test 3: Single Dash Arguments")
 
 		ProgramOptions testData;
 		testData.processSingleDashOptions(inputData);
+		testData.SetDefaultOptionsWhenNoFlags();
+		CHECK(expectData == testData);
+		if (expectData != testData)
+		{
+			checkWhatFailed(expectData, testData);
+		}
+	}
+}
+
+TEST_CASE("Program Options Unit Test 4: Double Dash Arguments")
+{
+	SUBCASE("Double Dash Arguments Sub Test 1")
+	{
+		// When a single output flag is set, only that output should be expected
+		ProgramOptions expectData;
+		std::string_view inputData = "--bytes";
+		expectData.byteCount = true;
+		expectData.useDefaultFlags = false;
+
+		ProgramOptions testData;
+		testData.processDoubleDashOptions(inputData);
+		testData.SetDefaultOptionsWhenNoFlags();
+		CHECK(expectData == testData);
+		if (expectData != testData)
+		{
+			checkWhatFailed(expectData, testData);
+		}
+	}
+
+	SUBCASE("Double Dash Arguments Sub Test 2")
+	{
+		// -R, recursive file search should not affect default flags
+		ProgramOptions expectData;
+		std::string_view inputData = "--subdirectories";
+		expectData.recurseSubDirectories = true;
+		expectData.byteCount = true;
+		expectData.lineCount = true;
+		expectData.wordCount = true;
+
+		ProgramOptions testData;
+		testData.processDoubleDashOptions(inputData);
+		testData.SetDefaultOptionsWhenNoFlags();
+		CHECK(expectData == testData);
+		if (expectData != testData)
+		{
+			checkWhatFailed(expectData, testData);
+		}
+	}
+
+	SUBCASE("Double Dash Arguments Sub Test 3")
+	{
+		// -t, time execution should not affect default flags
+		ProgramOptions expectData;
+		std::string_view inputData = "--time-execution";
+		expectData.enableExecutionTime = true;
+		expectData.byteCount = true;
+		expectData.lineCount = true;
+		expectData.wordCount = true;
+
+		ProgramOptions testData;
+		testData.processDoubleDashOptions(inputData);
+		testData.SetDefaultOptionsWhenNoFlags();
+		CHECK(expectData == testData);
+		if (expectData != testData)
+		{
+			checkWhatFailed(expectData, testData);
+		}
+	}
+}
+
+TEST_CASE("Program Options Unit Test 5: Process Arguments")
+{
+	SUBCASE("Process Argument Sub Test 1")
+	{
+		// When a single output flag is set, only that output should be expected
+		ProgramOptions expectData;
+		std::string_view inputData = "-L";
+		expectData.maxLineWidth = true;
+		expectData.useDefaultFlags = false;
+
+		ProgramOptions testData;
+		testData.processArgument(inputData);
+		testData.SetDefaultOptionsWhenNoFlags();
+		CHECK(expectData == testData);
+		if (expectData != testData)
+		{
+			checkWhatFailed(expectData, testData);
+		}
+	}
+
+	SUBCASE("Process Argument Sub Test 2")
+	{
+		// -R, recursive file search should not affect default flags
+		ProgramOptions expectData;
+		std::string_view inputData = "-R";
+		expectData.recurseSubDirectories = true;
+		expectData.byteCount = true;
+		expectData.lineCount = true;
+		expectData.wordCount = true;
+
+		ProgramOptions testData;
+		testData.processArgument(inputData);
+		testData.SetDefaultOptionsWhenNoFlags();
+		CHECK(expectData == testData);
+		if (expectData != testData)
+		{
+			checkWhatFailed(expectData, testData);
+		}
+	}
+
+	SUBCASE("Process Argument Sub Test 3")
+	{
+		// -t, time execution should not affect default flags
+		ProgramOptions expectData;
+		std::string_view inputData = "-t";
+		expectData.enableExecutionTime = true;
+		expectData.byteCount = true;
+		expectData.lineCount = true;
+		expectData.wordCount = true;
+
+		ProgramOptions testData;
+		testData.processArgument(inputData);
+		testData.SetDefaultOptionsWhenNoFlags();
+		CHECK(expectData == testData);
+		if (expectData != testData)
+		{
+			checkWhatFailed(expectData, testData);
+		}
+	}
+
+	SUBCASE("Process Argument Sub Test 4: Multiple Flags")
+	{
+		ProgramOptions expectData;
+		std::string_view inputData = "-RtLlc";
+		expectData.enableExecutionTime = true;
+		expectData.recurseSubDirectories = true;
+		expectData.byteCount = true;
+		expectData.lineCount = true;
+		expectData.maxLineWidth = true;
+		expectData.useDefaultFlags = false;
+
+		ProgramOptions testData;
+		testData.processArgument(inputData);
+		testData.SetDefaultOptionsWhenNoFlags();
+		CHECK(expectData == testData);
+		if (expectData != testData)
+		{
+			checkWhatFailed(expectData, testData);
+		}
+	}
+
+	SUBCASE("Process Argument Sub Test 5")
+	{
+		// When a single output flag is set, only that output should be expected
+		ProgramOptions expectData;
+		std::string_view inputData = "--bytes";
+		expectData.byteCount = true;
+		expectData.useDefaultFlags = false;
+
+		ProgramOptions testData;
+		testData.processArgument(inputData);
+		testData.SetDefaultOptionsWhenNoFlags();
+		CHECK(expectData == testData);
+		if (expectData != testData)
+		{
+			checkWhatFailed(expectData, testData);
+		}
+	}
+
+	SUBCASE("Process Argument Sub Test 6")
+	{
+		// -R, recursive file search should not affect default flags
+		ProgramOptions expectData;
+		std::string_view inputData = "--subdirectories";
+		expectData.recurseSubDirectories = true;
+		expectData.byteCount = true;
+		expectData.lineCount = true;
+		expectData.wordCount = true;
+
+		ProgramOptions testData;
+		testData.processArgument(inputData);
+		testData.SetDefaultOptionsWhenNoFlags();
+		CHECK(expectData == testData);
+		if (expectData != testData)
+		{
+			checkWhatFailed(expectData, testData);
+		}
+	}
+
+	SUBCASE("Process Arguments Sub Test 7")
+	{
+		// -t, time execution should not affect default flags
+		ProgramOptions expectData;
+		std::string_view inputData = "--time-execution";
+		expectData.enableExecutionTime = true;
+		expectData.byteCount = true;
+		expectData.lineCount = true;
+		expectData.wordCount = true;
+
+		ProgramOptions testData;
+		testData.processArgument(inputData);
 		testData.SetDefaultOptionsWhenNoFlags();
 		CHECK(expectData == testData);
 		if (expectData != testData)
